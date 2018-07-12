c91843c8672cca2540f56b113f77cf911dfb7491
Znasif <1305030.nz@ugrad.cse.buet.ac.bd>
2018-07-12 05:00:01 -0700

    Started Graphics4
    Signed-off-by: Znasif <1305030.nz@ugrad.cse.buet.ac.bd>

---
+++ Computer Graphics/Graphics 4/OpenGL_CodeBlocks/FILE2.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<vector>
+#include <windows.h>
+#include <glut.h>
+
+Object{
+    Vector3 reference_point;
+    Double height, width, length;
+    int Shine;
+    double color[3];
+    double co_efficients[4];
+    Object(){};
+    virtual void draw(){};
+    void setColor;
+    void setShine;
+    void setCoEfficients;
+}
+
+vector <Object>  objects;
+vector <Vector3> lights;
+
+Ray{
+    Vector3 start;
+    Vector3 dir;
+}
